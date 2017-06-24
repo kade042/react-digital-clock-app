6836f48efa7b1745b8db44c82360ca0902bc7891
kade042 <dereje88@gmail.com>
2017-06-24 14:50:12 -0600

    adding Digit component

---
+++ src/components/Digit.js
@@ -0,0 +1,30 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Digit extends Component {
+  render() {
+    let digitStyle = {
+      display: 'inline-block',
+      fontSize: 20,
+      padding: 10,
+      margin: 5,
+      background: '#eeeeee',
+    };
+
+    let displayValue;
+    if (this.props.value < 10) {
+      displayValue = '0' + this.props.value;
+    } else {
+      displayValue = this.props.value;
+    }
+
+    return (
+      <div style={digitStyle}>
+        {displayValue}
+      </div>
+    );
+  }
+}
+
+Digit.propTypes = {
+  value: propTypes.number.isRequired,
+};
